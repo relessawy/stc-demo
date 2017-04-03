12218d63571f810a123149af02505941ca98e4e0
wael <>
2017-04-03 19:45:24 +0400

    {/deduction/src/main/java/deduction/RuleValidation.java}

--- deduction/src/main/java/deduction/RuleValidation.java
+++ deduction/src/main/java/deduction/RuleValidation.java
@@ -9,28 +9,29 @@ import java.util.Map;
 
 public class RuleValidation implements java.io.Serializable
 {
-   
+
    static final long serialVersionUID = 1L;
 
-    private Map<String, String> englishMessages = new HashMap<String, String>();
-	private Map<String, String> arabicMessages = new HashMap<String, String>();
-       
+   private Map<String, String> englishMessages = new HashMap<String, String>();
+   private Map<String, String> arabicMessages = new HashMap<String, String>();
+
    private java.lang.String key;
 
+   private java.lang.String message;
 
    public RuleValidation()
    {
-       englishMessages.put("rule01","Error while fetching your data, Please try again later");
-       arabicMessages.put("rule01","عذرا حصل خطا اثناء محاولة استعادة بياناتك, الرجاء المحاولة في وقت لاحق");
-       
-       englishMessages.put("rule02","Deduction amount cannot be greater than net salary");
-       arabicMessages.put("rule02","المبلغ المراد خصمه لا يمكن ان يكون اكبر من اجمالي الراتب");
-       
-       englishMessages.put("rule03","");
-       arabicMessages.put("rule03","");
-       
-       englishMessages.put("rule04","");
-       arabicMessages.put("rule04","");
+      englishMessages.put("rule01", "Error while fetching your data, Please try again later");
+      arabicMessages.put("rule01", "عذرا حصل خطا اثناء محاولة استعادة بياناتك, الرجاء المحاولة في وقت لاحق");
+
+      englishMessages.put("rule02", "Deduction amount cannot be greater than net salary");
+      arabicMessages.put("rule02", "المبلغ المراد خصمه لا يمكن ان يكون اكبر من اجمالي الراتب");
+
+      englishMessages.put("rule03", "");
+      arabicMessages.put("rule03", "");
+
+      englishMessages.put("rule04", "");
+      arabicMessages.put("rule04", "");
    }
 
    public java.lang.String getKey()
@@ -41,18 +42,30 @@ public class RuleValidation implements java.io.Serializable
    public void setKey(java.lang.String key)
    {
       this.key = key;
+      
    }
 
-   public java.lang.String getMessage(String key,String lang)
+   public java.lang.String getMessage1(String key, String lang)
    {
-      return lang.equals("ar")?arabicMessages.get(key):englishMessages.get(key);
+      return lang.equals("ar") ? arabicMessages.get(key) : englishMessages.get(key);
    }
 
-   
-   public RuleValidation(java.lang.String key)
+
+   public java.lang.String getMessage()
+   {
+      return this.message;
+   }
+
+   public void setMessage(java.lang.String message)
+   {
+      this.message = message;
+   }
+
+   public RuleValidation(java.lang.String key, java.lang.String message)
    {
       this();
       this.key = key;
+      this.message = message;
    }
 
 }
