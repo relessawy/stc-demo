3dbe8234359cb002140e59ed620f780a67dfb2a9
wael <>
2017-04-07 10:02:40 +0400

    {/deduction/src/main/java/deduction/Employee.java}

--- deduction/src/main/java/deduction/Employee.java
+++ deduction/src/main/java/deduction/Employee.java
@@ -21,6 +21,10 @@ public class Employee implements java.io.Serializable
 
    private java.lang.String grade;
 
+   private java.lang.String fitnessTimeLastYear;
+
+   private int requestPending;
+
    public Employee()
    {
    }
@@ -65,13 +69,36 @@ public class Employee implements java.io.Serializable
       this.grade = grade;
    }
 
+   public java.lang.String getFitnessTimeLastYear()
+   {
+      return this.fitnessTimeLastYear;
+   }
+
+   public void setFitnessTimeLastYear(java.lang.String fitnessTimeLastYear)
+   {
+      this.fitnessTimeLastYear = fitnessTimeLastYear;
+   }
+
+   public int getRequestPending()
+   {
+      return this.requestPending;
+   }
+
+   public void setRequestPending(int requestPending)
+   {
+      this.requestPending = requestPending;
+   }
+
    public Employee(java.lang.String id, java.lang.String name, int salary,
-         java.lang.String grade)
+         java.lang.String grade, java.lang.String fitnessTimeLastYear,
+         int requestPending)
    {
       this.id = id;
       this.name = name;
       this.salary = salary;
       this.grade = grade;
+      this.fitnessTimeLastYear = fitnessTimeLastYear;
+      this.requestPending = requestPending;
    }
 
 }
